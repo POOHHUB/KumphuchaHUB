ba4f6743394f30f8eeb1a924c83671308aedd1a4
POOHHUB <128593892+POOHHUB@users.noreply.github.com>
2023-08-20 13:46:31 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,3 @@
 local mapplay = "https://raw.githubusercontent.com/POOHHUB/KumphuchaHUB/main/KM.lua"
+loadstring(game:HttpGet(mapplay,true))()
 
-local mappay = game.PlaceId
-if mappay = 2753915549 or 4442272183 or 7449423635 then 
-  loadstring(game:HttpGet(mapplay,true))()
-end
