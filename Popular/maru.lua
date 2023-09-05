308922b3ab92f00cc9c386251f9016aa3c4a722a
POOHHUB <128593892+POOHHUB@users.noreply.github.com>
2023-09-05 11:14:59 +0700

    Create maru.lua

---
+++ Popular/maru.lua
@@ -0,0 +1,3 @@
+(getgenv()).key = "MARUK6FHH6DLYGVUD31YBQFX4E";
+(getgenv()).id = "928471250412658769";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruComkak/main/PCBit.lua"))()
