3cb9c0f274824b3f87b4d55dc0a7807e7746ff80
POOHHUB <128593892+POOHHUB@users.noreply.github.com>
2023-10-09 14:51:16 +0700

    Update FatesAdmin.lua

--- Popular/FatesAdmin.lua
+++ Popular/FatesAdmin.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/fatesc/fates-admin/main/main.lua", "FatesAdmin"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fatesc/fates-admin/main/main.lua"))()
